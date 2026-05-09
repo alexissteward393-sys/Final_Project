d03ab870f8e5eaa576ea318decd8521588275dd0
Alexis Steward <alexissteward393@gmail.com>
2026-05-09 14:14:56 -0500

    Fixed error with isfile and join. Changed from os import to from os.path import.

--- src/project.py
+++ src/project.py
@@ -3,7 +3,7 @@ import random
 import math
 import pygame
 from os import listdir
-from os import isfile, join
+from os.path import isfile, join
 pygame.init()
 
 pygame.display.set_caption("Platformer")
