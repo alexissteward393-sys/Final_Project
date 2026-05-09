78c9ea1fc93f08529dc60def37ba841ca68b0d34
Alexis Steward <alexissteward393@gmail.com>
2026-05-09 14:05:31 -0500

    Set up the general game info and window.

--- src/project.py
+++ src/project.py
@@ -6,3 +6,11 @@ from os import listdir
 from os import isfile, join
 pygame.init()
 
+pygame.display.set_caption("Platformer")
+
+bg_color = (255, 255, 255)
+width, height = 800, 500
+fps = 60
+player_vel = 5
+
+window = pygame.display.set_mode((width, height))
