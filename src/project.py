7f097d3560ad83fd7f218ae7a82a24190dbcb126
Alexis Steward <alexissteward393@gmail.com>
2026-05-09 15:24:23 -0500

    Started player class and player movement.

--- src/project.py
+++ src/project.py
@@ -9,11 +9,34 @@ pygame.init()
 pygame.display.set_caption("Platformer")
 
 bg_color = (255, 255, 255)
-width, height = 800, 500
+width, height = 1200, 650
 fps = 60
 player_vel = 5
 
 window = pygame.display.set_mode((width, height))
+
+class Player(pygame.sprite.Sprite):
+    color = (255, 0, 0)
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+
 
 def main(window):
     clock = pygame.time.Clock()
